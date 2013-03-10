c904dc37fd9e128634b01a10569aa40dc68c2189
softprops <d.tangren@gmail.com>
2013-03-09 23:49:08 -0500

    spacing

--- src/main/scala/iso8601.scala
+++ src/main/scala/iso8601.scala
@@ -1,7 +1,7 @@
 package iso8601
 
 object Iso8601 {
- def apply(in: String) = Parse(in) match {
+  def apply(in: String) = Parse(in) match {
     case Parse.Success(dt, _) => Right(dt)
     case _ => Left("malformed date: %s" format in)
   }
